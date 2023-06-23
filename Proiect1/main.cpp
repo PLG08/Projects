12666582cb3a35d6dbef3035d6f958e06a6bd45f
Alex Pirlog <alexpirlog11@gmail.com>
2023-06-23 20:50:06 +0300

    pregatire1

---
+++ Proiect1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a=1, suma=0;
+    while (a!=0)
+    {
+        cin >> a;
+        suma=suma+a;
+        cout << suma << endl;
+    }
+    return 0;
+}
