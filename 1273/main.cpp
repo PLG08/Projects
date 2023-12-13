d39bd0d4b2480d0fc11fb890b6c7ee764b8d2f90
Alex Pirlog <alexpirlog11@gmail.com>
2023-12-13 18:10:13 +0200

    Tema-Scoala- 13.12.2023

---
+++ 1273/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x,y,s,uc;
+    cin>> x;
+    cin>> y;
+    s=x+y;
+    uc=s%10;
+
+    cout << uc << endl;
+    return 0;
+}
---
+++ 939/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int s,a,b;
+    cin>>a;
+    cin>>b;
+
+    s=a+b;
+
+    cout << s << endl;
+    return 0;
+}
